c5ffb6f90783f82c69064c4eede2306a60687538
BAHLEL <arslane.bahlel@capgemini.com>
2019-10-17 10:41:21 +0200

    First commit

---
+++ src/tp/bdd/externalsort/main/Sort.java
@@ -0,0 +1,34 @@
+package tp.bdd.externalsort.main;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Sort {
+	public int pageSize;
+	public int bufferSize;
+	public String inputFile;
+	public String outputFile;
+	
+	public void getFromFile() throws FileNotFoundException {
+			Scanner sc = new Scanner(new BufferedReader(new FileReader("C:\\Users\\abahlel\\eclipse-workspace\\ExternalSort\\src\\tp\\bdd\\externalsort\\main\\in.txt")));
+			while(sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		
+	}
+	
+
+	public List arrayToArrays(int sublistsize, List<Integer> mylist) {
+		List<List<Integer>> ba3oucha = new ArrayList<List<Integer>>();
+		
+		return ba3oucha;
+	}
+	public static void main(String[] args) {
+		
+		}
+	}
+
