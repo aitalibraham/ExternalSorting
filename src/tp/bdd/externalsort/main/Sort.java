0c3a827fc5220f71a9b216ba3f0201b9e92255b6
BAHLEL <arslane.bahlel@capgemini.com>
2019-10-20 17:55:14 +0200

    merge fixed

--- src/tp/bdd/externalsort/main/Sort.java
+++ src/tp/bdd/externalsort/main/Sort.java
@@ -1,12 +1,14 @@
 package tp.bdd.externalsort.main;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -18,7 +20,49 @@ public class Sort {
 	public String inputFile;
 	public String outputFile;
 	
-	public void Sorter() throws FileNotFoundException {
+	public void Sorter() throws IOException {
+		divideFileIntoFiles();
+		String[] files = new File("buffer").list();
+		for(int i=0;i<files.length-1;i++) {
+			mergeCouple(files[i], files[i+1]);
+		}
+		new File("buffer\\"+files[files.length-1]).renameTo(new File("buffer\\out.txt"));
+	}
+
+
+	public void mergeCouple(String firstFile, String secondFile) throws IOException {
+		BufferedReader firstReader = new BufferedReader(new FileReader("buffer\\"+firstFile));
+		BufferedReader secondReader = new BufferedReader(new FileReader("buffer\\"+secondFile));
+		String firstLine=firstReader.readLine(), secondLine=secondReader.readLine();
+		int firstInt, secondInt;
+		List mylist = new ArrayList<Integer>();
+		while(firstLine!=null) {
+			firstInt=Integer.parseInt(firstLine);
+			if(secondLine!=null) {
+				secondInt=Integer.parseInt(secondLine);
+				if(firstInt<secondInt) {
+					mylist.add(firstInt);
+					firstLine=firstReader.readLine();
+				}else {
+					mylist.add(secondInt);
+					secondLine=secondReader.readLine();
+				}
+			}else {
+				mylist.add(firstInt);
+				firstLine=firstReader.readLine();;
+			}
+		}
+		while((secondLine=firstReader.readLine())!=null) {	
+			secondInt=Integer.parseInt(secondLine);
+			mylist.add(secondInt);
+			}
+		arrayListToFile("buffer\\"+secondFile, mylist);
+		firstReader.close();
+		secondReader.close();
+		new File("buffer\\"+firstFile).delete();
+	}
+	
+	public void divideFileIntoFiles() throws IOException {
 		BufferedReader objReader = new BufferedReader(new FileReader("SourceFiles\\in.txt"));
 		String line;
 		List l=new ArrayList<Integer>();;
@@ -30,23 +74,19 @@ public class Sort {
 				if(linecount % 6 == 0) {
 					Collections.sort(l);
 					arrayListToFile("buffer\\"+(linecount/6)+".txt", l);
-					l=new ArrayList<Integer>();
+					l.clear();
 				}
 			}
-			
 			if(!l.isEmpty()) {
 				Collections.sort(l);
 				arrayListToFile("buffer\\"+(((int) linecount/6)+1)+".txt", l);
 			}
+			
 				
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-	}
-	
-	public List fileToArrayList(String filename) throws IOException {
-		
+		objReader.close();
 	}
 	
 	public void arrayListToFile(String filepath, List<Integer> mylist) throws IOException {
@@ -57,7 +97,7 @@ public class Sort {
 			writer.close();
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		try {
 			new Sort().Sorter();
 		} catch (FileNotFoundException e) {
