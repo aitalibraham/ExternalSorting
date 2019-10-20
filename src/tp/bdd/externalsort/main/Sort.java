59476989e5e4e45bd25d7b45de5d978241dda1b9
aitalibraham <farouk.alibraham@gmail.com>
2019-10-20 20:27:06 +0200

    last changes

--- src/tp/bdd/externalsort/main/Sort.java
+++ src/tp/bdd/externalsort/main/Sort.java
@@ -1,11 +1,18 @@
 package tp.bdd.externalsort.main;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
+import sun.font.CreatedFontTracker;
 
 public class Sort {
 	public int pageSize;
@@ -21,8 +28,13 @@ public class Sort {
 		
 	}
 	
-	public void putToFile(String fileName, List<Integer> sortedListe) {
+	public void putToFile(String fileName, List<Integer> sortedList) throws IOException {
+		PrintWriter pr = new PrintWriter(fileName);
 		
+		for (int num:sortedList)
+			pr.println(num);
+		
+		pr.close();
 	}
 	
 	
@@ -32,7 +44,13 @@ public class Sort {
 		return ba3oucha;
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		Sort srt = new Sort();
+		List<Integer> yanisList =  new ArrayList<Integer>();
+		yanisList.add(13);
+		yanisList.add(3);
+		yanisList.add(1);
+		srt.putToFile("yanis.txt" , yanisList);
 		
 	}
 }
